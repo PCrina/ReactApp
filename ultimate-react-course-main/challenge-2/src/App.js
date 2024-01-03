0b55f19788cac849e68d22027aa7a177e6701097
crina.putanu <crina.putanu@SVROLP03767>
2024-01-03 12:37:42 +0200

    challenge-2: Using ObjectsList and distructuring props

---
+++ ultimate-react-course-main/challenge-2/src/App.js
@@ -0,0 +1,77 @@
+import "./App.css";
+
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "intermediate",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "beginner",
+    color: "#EFD81D",
+  },
+  {
+    skill: "React",
+    level: "beginner",
+    color: "#60DAFB",
+  },
+  {
+    skill: "C#",
+    level: "intermediate",
+    color: "#FF3B00",
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="images.jpeg" alt="Avatar" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Putanu Crina</h1>
+      <p>
+        Software engineer with some years experience in C# technologies, both FE
+        and BE.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      {skills.map((skill) => (
+        <Skill skill={skill.skill} color={skill.color} level={skill.level} />
+      ))}
+    </div>
+  );
+}
+
+function Skill({ skill, color, level }) {
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      <span>
+        {level === "beginner" && "👶"}
+        {level === "intermediate" && "👍"}
+        {level === "advanced" && "💪"}
+      </span>
+    </div>
+  );
+}
+
+export default App;
